cf6654c1e06fafb580c2e64f5e30f8cf856f63da
RAJ SONAWANE <86197396+Raj96659@users.noreply.github.com>
2025-04-07 12:09:54 +0530

    Create A_Basic.py

---
+++ A_Basic.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Read data from csv file into a dataframe
+df = pd.read_csv("C:\\Users\\Jay Sunil Sonawane\\Desktop\\Raj Excel\\Cricket_Player_Data.csv", encoding="utf-8")
+
+print(df)
